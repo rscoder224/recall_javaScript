bf073046fd08725b563c7b45d772558be68489a8
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-01 20:05:32 +0530

    Create 04_objects_in.js
    objects in javascript

---
+++ 04_objects_in.js
@@ -0,0 +1,51 @@
+// Arrays are good but not sufficient for real world data
+// objects strore key value pairs;
+// Objects Store key value pairs object don't have index
+
+// how to create objects in JavaScript 
+
+// How access data from objects
+
+// how add key value pair in objects
+
+const obj = {
+    name: "shaurya",
+    section : "CSE45"
+}
+
+obj.taau = "no Taau";
+
+
+console.log(obj.taau);
+
+console.log(obj["taau"]);
+
+obj.band = "Khewaari Band";
+
+console.log(obj);
+
+
+// Difference Between dot and bracket notation 
+
+const key = "email";
+
+const person = {name: "Shaurya",
+    age : 22,
+    "phone Number" : 9928283744
+}
+
+person[key] = 'ltstehhegh@gamil.com';
+
+console.log(person);
+
+
+
+
+
+
+
+
+
+
+
+
