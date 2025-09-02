163836e07f3894a63672ecc09701b07b5bd6be02
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-02 16:50:42 +0530

    Create 15_Block_scope_vs_function_scope.js
    block Scope Vs Function Sope

---
+++ 15_Block_scope_vs_function_scope.js
@@ -0,0 +1,20 @@
+// block scope Vs Function Scope
+
+// let const are block scope
+// var is function scope
+
+{
+    var FirstName = "Kallu rana"
+let firstName = "Shaurya";    
+}
+
+console.log(FirstName.trim().length-1);
+
+
+
+
+
+{
+    
+}
+
