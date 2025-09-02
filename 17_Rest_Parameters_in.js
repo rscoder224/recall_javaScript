c648d7c0f29a69c64cc91bcea1c6750ddf16bd6e
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-02 17:34:37 +0530

    Update 17_Rest_Parameters_in.js
    Rest Parameters

--- 17_Rest_Parameters_in.js
+++ 17_Rest_Parameters_in.js
@@ -8,3 +8,20 @@ function add(a,b,...c){
 
 
 add(1,2,3,4,5,6,7);
+
+
+function addAll(...numbers){
+    let total = 0;
+    for(let number of numbers){
+        total = total + number;
+    }
+    return total;
+}
+
+let ans = addAll(1,2,3,4,5,6,7,8,9,10);
+
+console.log(ans);
+
+
+
+
