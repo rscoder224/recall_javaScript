bd99b45085c52154843a6528eb74e724b1406625
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-02 14:28:42 +0530

    Create 09_objects_inside_array.js
    objects inside Array();

---
+++ 09_objects_inside_array.js
@@ -0,0 +1,38 @@
+// object Inside the Array
+// very useful for RealWorld Data
+
+
+const users = [
+    {name: "Shaurya", age:22 , 'contact number' : 9938384838 , product :'iphone'},
+    {name: "Atish", age:19 , 'contact number' : 66238384838 , product :'iphone'},
+    {name: "Surendra", age:56 , 'contact number' : 3383849833 , product :'iphone'},
+    {name: "Ramesh", age:12 , 'contact number' : 7938384838 , product :'iphone'}
+    ];
+    
+    
+    // console.log(users[0].name);
+    
+    for(let i = 0 ; i < users.length ; i++){
+        console.log(users[i]);
+    };
+    
+    // console.log(users);
+    
+    
+    // for(let user of users){
+    //     console.log(user);
+    // }
+    
+    // for(let user in users){
+    //     console.log(users[user]);
+    // }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
