bd93a5bfdd7e4bf3615c0edfe7d2da4349e11885
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-02 15:01:28 +0530

    Create 12_Hoisting_in.js
    Hoisting in JavaSCript

---
+++ 12_Hoisting_in.js
@@ -0,0 +1,19 @@
+// Hoisting In JavaScript
+// if I talk About hoisting it means that function can be called before declaration;
+{
+console.log(add(2,9));
+
+
+
+
+
+function add(a,b){
+    return a+b;
+}
+
+}
+
+// console.log(hello);
+
+
+// var hello = 'hello world';
