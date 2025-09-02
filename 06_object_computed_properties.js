8c9916faa2ad84b2135b0bbe733ae65a298246a2
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-02 13:35:48 +0530

    Create 06_object_computed_properties.js
    06_object_computed_properties.js

---
+++ 06_object_computed_properties.js
@@ -0,0 +1,22 @@
+// computed properties
+// computed properties of an Object;
+
+
+const key1 = "obj";
+const key2 = "obj2";
+
+const var1 = 'value1';
+const var2 = 'value2';
+
+const obj = {};
+
+console.log(obj);
+
+obj[key1] = var1;
+obj[key2] = var2;
+
+console.log(obj);
+
+
+
+ 
