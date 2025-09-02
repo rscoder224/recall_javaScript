6f84e36aee622bc2f8969ee6b89061020999d822
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-02 13:53:04 +0530

    Create 07_Spread_operators_in.js
    Spread Operators In JS

---
+++ 07_Spread_operators_in.js
@@ -0,0 +1,47 @@
+
+// spread Operater in array
+// InCase of Array
+// const arr1 = [1,2,3,4];
+
+// const arr2 = [5,6,7,8];
+
+// const newArr = [...arr1, ...arr2, 9, ...'abcdefghijklmnopqrstuvwxyz'];
+
+// console.log(newArr); 
+
+// Spread operators in Case of Objects
+
+const obj1 = {
+    key1 : "value1",
+    key2 : "value2",
+}
+
+
+const obj2 = {
+    key3 : "value3",
+    key4 : "value4",
+}
+
+
+
+const newObj = {...'abcdefghijklmnopqrstuvwxyz'};
+
+
+console.log(newObj);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
