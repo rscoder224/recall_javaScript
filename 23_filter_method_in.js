fc97359a2cc545b4ff5a399a6cae41bac01411ab
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-03 15:12:30 +0530

    Update 23_filter_method_in.js
    filter in JS

--- 23_filter_method_in.js
+++ 23_filter_method_in.js
@@ -1,3 +1,4 @@
+
 // filter method of array
 // ye boolean value generate karta agar condition true huyi
 // toh new array generate hogi aur usme saara true conditon wale 
@@ -13,5 +14,30 @@ const fil = function(num){
 const ans = number.filter(fil);
 
 console.log(ans);
+
+// real world Example
+
+
+
+    const users = [
+    {name : "Shaurya", Product: "Iphone", id:23433, price:55400},
+    {name : "Divyanshu", Product: "Laptop", id:67685, price:66000},
+    {name : "Ayush", Product: "Desk", id:23433, price:15400},
+    {name : "Parth", Product: "Motrola", id:23459, price:25400},
+    {name : "Himanshu", Product: "Earbuds", id:234343, price:5400}
+    ];
+    
+    const res = users.filter((item)=>{
+        return item.price >15000;
+    });
+    
+    console.log(res);
+    
+    
+    
+
+
+
+
 
 
