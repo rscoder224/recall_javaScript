1a4689de4b3db745febaa1a4ff182921302a6cc3
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-05 23:47:26 +0530

    Create 28_some_method_array.js
    Some Inside an array in JavaScript.

---
+++ 28_some_method_array.js
@@ -0,0 +1,26 @@
+// some method inside array 
+
+const numbers = [2,3,4,5];
+// kya ek bhi number aisa hai jo even hai 
+// ek baar bhi conditon true hone pe true return karega
+
+const ans = numbers.some((number)=> number%2 === 0);
+
+console.log(ans);
+
+
+const users = [
+{userId:1 , userName: "Shaurya" , product:"Iphone", price: 60000},
+{userId:2 , userName: "Kunal", product: "laptop", price: 80000},
+{userId:3 , userName: "rajesh", product: "Phone Cover", price: 200},
+{userId:4 , userName: "abhay", product: "game console", price: 900},
+{userId:5 , userName:"nitesh" ,product: "ps3", price:25000},
+{userId:6 , userName : "kallu", product: "guitar",price:5000},
+];
+
+
+const ans2 = users.some((user)=>{
+    return user.price <= 200;
+})
+
+console.log(ans2);
