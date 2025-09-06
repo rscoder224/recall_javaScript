c8c3134a72f32f91bcad807fac7ad9a60f999072
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-06 22:02:35 +0530

    Create 30_splice_method.js
    splice method of array

---
+++ 30_splice_method.js
@@ -0,0 +1,12 @@
+// splice method of array 
+// Start Index , no. of items want to Delete , Insert
+
+
+const myArray = ["item","item2","item3","item4"];
+myArray.splice(1,1);
+
+
+
+myArray.splice(1,0,"item2");
+
+console.log(myArray);
