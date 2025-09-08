a03be8f3a8243c8916927a431676c4a47dbc043f
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-08 09:01:31 +0530

    Update 28_some_method_array.js
    Some Method inside The Array.

--- 28_some_method_array.js
+++ 28_some_method_array.js
@@ -1,8 +1,10 @@
 // some method inside array 
 
-const numbers = [2,3,4,5];
+const numbers = [3,5];
 // kya ek bhi number aisa hai jo even hai 
 // ek baar bhi conditon true hone pe true return karega
+// ye boolean return Karega;
+
 
 const ans = numbers.some((number)=> number%2 === 0);
 
@@ -15,12 +17,11 @@ const users = [
 {userId:3 , userName: "rajesh", product: "Phone Cover", price: 200},
 {userId:4 , userName: "abhay", product: "game console", price: 900},
 {userId:5 , userName:"nitesh" ,product: "ps3", price:25000},
-{userId:6 , userName : "kallu", product: "guitar",price:5000},
+{userId:6 , userName : "kallu", product: "guitar",price:5000}
 ];
-
 
 const ans2 = users.some((user)=>{
     return user.price <= 200;
-})
+});
 
 console.log(ans2);
