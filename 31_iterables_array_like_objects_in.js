151b30f06985330d941ead4d1fb5585e683f4964
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-08 09:30:03 +0530

    Create 31_iterables_array_like_objects_in.js
    iterables inside the array and array like objects in the array

---
+++ 31_iterables_array_like_objects_in.js
@@ -0,0 +1,24 @@
+
+// Iteables 
+// Jispe hum for of laga skate hai 
+// string and array
+
+const string = 'Shaurya';
+
+console.log(string);
+
+for(let word of string){
+    console.log(word);
+}
+
+const array = ["item1",'item2','item3'];
+
+ for(let item of array){
+    console.log(item);
+}
+
+// array like objects
+// jinke pas length property hoti hai 
+// aur jinko hum index se access kar sakte hai 
+// example --> string
+
