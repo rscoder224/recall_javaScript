f07dac59ad94818f01ef8f1b196847f2e58c8db3
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-08 09:23:07 +0530

    Update 30_splice_method.js
    splice method inside an Array

--- 30_splice_method.js
+++ 30_splice_method.js
@@ -26,3 +26,29 @@ const array4 = [1,2,3,4,5,6,7,8,9];
 array4.splice(0,3,"babu",'raju');
 
 console.log(array4);
+
+
+
+
+// splice method of an Array 
+// in This method we can Delete as well as insert element at a time 
+// inside an array 
+// splice method se hum kisi bhi existing array ko modify kar sakte hai
+// usme Item delete ya insert kar sakte hai
+// ishke liye hame
+// array.splice(start_index,no_of_item_want_to_delete,"item_want_to_insert");
+
+// we can also store deleted item later
+
+const array4 = [1,2,3,4,5,6,7,8,9];
+
+
+const deleted_items = array4.splice(0,3,"babu",'raju');
+
+
+console.log(array4);
+
+console.log(deleted_items);
+
+
+
