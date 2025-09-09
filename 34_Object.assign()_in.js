593acf254cc9c56f2ec462a3db6347e360a383d7
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-09 09:58:19 +0530

    Create 34_Object.assign()_in.js
    Object.assign() in JavaScript

---
+++ 34_Object.assign()_in.js
@@ -0,0 +1,18 @@
+// Object cloning by Using object.Assign
+
+
+const obj = {
+    key1: "value1",
+    key2: 'Value2'
+}
+
+// const obj2 = Object.assign({},obj)
+
+
+const obj2 = Object.assign({},obj);
+
+
+
+console.log(obj2);
+
+
