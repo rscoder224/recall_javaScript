3568f46410ea2bddd35249300c6608bbdc67cf9a
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-09 10:40:31 +0530

    Create 37_use_strict&this_output.js
    use strict and this output in js

---
+++ 37_use_strict&this_output.js
@@ -0,0 +1,16 @@
+console.log(this);
+
+
+    console.log(this);
+
+
+    function myfunc(){
+"use strict"
+        console.log("hello Im there inside ");
+        console.log(this);
+
+
+    }
+
+
+myfunc();
