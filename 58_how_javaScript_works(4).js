cea8c304796f0252c41471450514b61acf8220e2
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-12 11:14:37 +0530

    Create 58_how_javaScript_works(4).js

---
+++ 58_how_javaScript_works(4).js
@@ -0,0 +1,15 @@
+                                                                                                                                                                                                                                                                                                                                                                                 
+// let and const GEC ("Global Execution Context");
+
+// Creation Phase 
+// window {}
+// this == window
+// firstName = Unintilized
+
+let firstName = "Shaurya";
+
+console.log(firstName);
+
+
+
+
