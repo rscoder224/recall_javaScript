0dfc7f4ab2f5ca21100e3015ac7be07afa1b68e7
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-13 22:18:22 +0530

    Create 74_clone_nodes.js

---
+++ 74_clone_nodes.js
@@ -0,0 +1,13 @@
+// clone nodes
+
+// const ul = document.querySelector('todo-list');
+
+// const li = document.createElement('li');
+
+// li.textContent = 'New Todo boy';
+
+// ul.append(li);
+
+// const li2 = li.cloneNode(true);
+
+// ul.prepend(li2);
