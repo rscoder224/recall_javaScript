f39320dadcb4e5a5c60158293c8040579c6d405d
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-13 22:09:59 +0530

    Create 66_get_element_by_class_name.js

---
+++ 66_get_element_by_class_name.js
@@ -0,0 +1,12 @@
+// get multiple items using getElement by class name
+// get multiple items using querySelectorAll
+
+// const nav_items = document.getElementsByClassName('nav-item');
+
+// console.log(Array.isArray(nav_items));
+
+
+
+const nav_items = document.querySelectorAll('a');
+
+// console.log(nav_items);
