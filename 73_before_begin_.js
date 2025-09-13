9e5402c7c0c956bf2cd2a1bdf3977692265d5812
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-13 22:17:50 +0530

    Create 73_before_begin_.js

---
+++ 73_before_begin_.js
@@ -0,0 +1,10 @@
+// insertAdjacentHTML Example: 
+// 'beforebegin'
+// 'afterbegin'
+// 'beforeend'
+// afterend'
+
+
+// const todoList = document.querySelector('.todo-list');
+// todoList.insertAdjacentHTML('beforebegin', '<h2> This is a new Todo List </h2>');
+
