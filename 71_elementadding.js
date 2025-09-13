5fb5d769a4b041aaa0f6d1964d0efadc93970822
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-13 22:15:54 +0530

    Create 71_elementadding.js

---
+++ 71_elementadding.js
@@ -0,0 +1,10 @@
+
+const todoList = document.querySelector('.todo-list')
+
+// todoList.innerHTML = todoList.innerHTML ;
+// todoList.innerHTML += ' <li> New Todo </li>';
+// todoList.innerHTML += ' <li> New Todo 2 </li>';
+// todoList.innerHTML += ' <li> New Todo 3 </li>';
+
+// when you should use it;
+
