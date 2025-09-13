4448ecec34538970e14457526b0be7a50647e9e0
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-13 22:14:11 +0530

    Create 69_dom_traversing.js

---
+++ 69_dom_traversing.js
@@ -0,0 +1,46 @@
+// // 
+
+// const rootnode = document.getRootNode();
+
+// // console.log(rootnode);
+
+// const htmlElementNode = rootnode.childNodes[0];
+
+// console.log(htmlElementNode.childNodes);
+
+
+const rootnode = document.getRootNode();
+
+const htmlElementNode = rootnode.childNodes[0];
+
+console.log(htmlElementNode.childNodes[0]);
+
+// const headElementNode = htmlElementNode.childNodes[0];
+// const textnode = htmlElementNode.childNodes[1];
+// const bodyElementNode = htmlElementNode.childNodes[2];
+
+
+// console.log(textnode.parentElement);
+
+// console.log(headElementNode.nextSibling);
+// console.log(headElementNode.nextElementSibling);
+
+// console.log(headElementNode.childNodes);
+
+// const hed = document.querySelector('h1');
+
+// const hedp = hed.parentNode.parentNode;
+
+
+// hedp.style.backgroundColor = 'lightblue';
+
+
+// const head = document.querySelector('head');
+
+// const tittle = head.querySelector('title');
+
+// console.log(tittle);
+// console.log(tittle.parentNode);
+
+
+
