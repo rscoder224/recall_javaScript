900af7bec9bb316b15815d6a6406f34bc0cebd1c
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-13 22:12:27 +0530

    Create 68_inner_html.js

---
+++ 68_inner_html.js
@@ -0,0 +1,9 @@
+// innerhtml
+
+// const headline = document.querySelector(".headline");
+
+// console.log(headline.innerHTML)
+
+// headline.innerHTML = '<h2> This is something else </h2> <p> This is a paragraph </p>'
+
+// /"" 
