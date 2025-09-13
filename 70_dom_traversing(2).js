c83d7f59695bc28ea7664f8c4bd8a74ae521c6d8
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-13 22:15:13 +0530

    Create 70_dom_traversing(2).js

---
+++ 70_dom_traversing(2).js
@@ -0,0 +1,25 @@
+const sectiontodo = document.querySelector('.section-todo');
+
+console.log(sectiontodo.classList);
+// console.log(sectiontodo);
+
+// for adding a class to class list
+// sectiontodo.classList.add('bg-dark');
+
+// for removing a class from class list
+// sectiontodo.classList.remove('bg-dark');
+
+// check if a class is present in class list
+// sectiontodo.classList.contains('container'); 
+
+// toggle will add the class if not present and remove the class if present
+// sectiontodo.classList.toggle('bg-dark');
+
+// const header = document.querySelector('.header');
+
+// // console.log(header.classList);
+
+// header.classList.add('bg-dark');
+
+
+
