f441bf963d863efb379dac39bd7de5a6583da6ec
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-13 22:05:36 +0530

    Create 63_dom_.js

---
+++ 63_dom_.js
@@ -0,0 +1,12 @@
+// change text
+// textContent property and innerText property
+// textContent - will get all the text including hidden text
+// innerText - will get only visible text
+const element = document.getElementById('main-heading');
+
+// element.textContent = 'This is something else';
+
+// console.log(element);
+
+// element.innerText = 'This is Mobile devices';
+
