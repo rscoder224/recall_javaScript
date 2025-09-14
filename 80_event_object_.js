39b38e0edd5ace1295eb513e5d986c8c9f5ead97
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-14 21:51:38 +0530

    Create 80_event_object_.js

---
+++ 80_event_object_.js
@@ -0,0 +1,25 @@
+// Event Object in javaScript
+
+// const firstbtn = document.querySelector('#one');
+const buttons = document.querySelectorAll('.my-buttons button');
+
+// firstbtn.addEventListener('click', function(){
+//     console.log();
+// });
+
+// jab bhi mai kisi bhi element pe event listener add hoga 
+// javascript Engine ==> line by line execution
+// browser ==> js Engine + extra features
+// browser ==> js engine + WebApi
+// jab browser ko pata chala ki user ne event proform 
+// browser do kaam karega
+// 1. callback function js engine ko dega
+// 2. call function ke sath us event ki infomation bhi dega
+
+// for(let button of buttons){
+//   button.addEventListener('click', (e)=>{
+//     console.log(e.currentTarget.textContent);
+//   });
+// }
+
+
