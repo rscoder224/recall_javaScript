4ae0f22d017f926fcc8d94f828cff580bded52ff
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-15 21:41:59 +0530

    Create 89_setInterval().js

---
+++ 89_setInterval().js
@@ -0,0 +1,18 @@
+// set interval
+
+// const int1 = Math.floor(Math.random()*10000);
+// const int2 = Math.floor(Math.random()*100);
+// console.log(int1);
+
+// 
+// console.log(int1,int2);
+
+// setInterval(()=>{
+//     const int1 = Math.floor(Math.random()*10000);
+//     if(int1 > 1024){
+//     console.log(int1);
+//     }
+// },2000);
+
+
+
