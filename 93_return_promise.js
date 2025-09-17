a9561dd75840583a48ce79edac380d0643685384
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-17 21:49:59 +0530

    Create 93_return_promise.js

---
+++ 93_return_promise.js
@@ -0,0 +1,31 @@
+
+// function returnPromise(){
+//     const Friedrice = ["vegetables","oil","salt","rice"];
+//     return new Promise((resolve,reject)=>{
+//       if(Friedrice.includes("vegetables") && Friedrice.includes("oil") && Friedrice.includes("salt") && Friedrice.includes("rice")){
+//         resolve("Friedrice");
+// }else{
+//     reject("Couldn't do it");}
+// })}
+
+
+
+// function returnPromise(){
+//     const Friedrice = ["vegetables","oil","salt","rice"];
+//     return new Promise((resolve,reject)=>{
+//         if(Friedrice.includes("vegetables")&&Friedrice.includes("oil")&&Friedrice.includes("salt")&&Friedrice.includes("rice")){
+//             resolve(" Yeyy yummy fried rice are ready to eat");
+//         }else{
+//             reject("Couldn't do it");
+//         }
+//     })
+// }
+
+
+// returnPromise().then((yes)=>{
+//     console.log(yes);
+// }).catch((no)=>{
+//     console.log(no);
+// })
+
+
