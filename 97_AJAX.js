f129beaf44b98af8b67ffdd3bdcd7ef47a5547be
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-17 21:52:31 +0530

    Create 97_AJAX.js

---
+++ 97_AJAX.js
@@ -0,0 +1,24 @@
+// Basic Theory 
+
+// AJAX stands for Asynchronous JavaScript And XML
+
+// is a set of web development techniques
+// using many web technologies on the client-side
+// to create asynchronous web applications.
+
+// With AJAX, web applications can send and retrieve data from a server asynchronously
+// data from a server asynchronously
+// (in the background)
+// without interfering with the display and behavior of the existing page.
+
+// we don't use XML now a days
+// we use JSON now a days
+
+
+// we have 3 most common ways to create and send request to the server 
+// 1) XMLHttpRequest (old way of doing it)==>(XHR)
+// 2) Fetch API (modern way of doing it)
+// 3) Axios (library for making HTTP requests)
+
+
+
